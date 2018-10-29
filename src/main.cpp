6ea7cd4243a3d84c8a5ed29123a0ab7fb7cf213d
crumblingstatue <radiantstatue@gmail.com>
2018-10-29 22:32:10 +0100

    Restore Autismo Fields to its former glory

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,101 @@
 #include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_mixer.h"
 
 #include "window.hpp"
 #include "game.hpp"
 #include "scene.hpp"
 
+#include <iostream>
+
 using namespace cayv;
 
-class TestScene : public Scene
+struct Pos {
+    int x, y;
+};
+
+constexpr Pos flowers[] = {
+    {3, 5},
+    {9, 4},
+    {2, 3},
+    {5, 3},
+    {1, 1},
+};
+
+class AutismoFields : public Scene
 {
 public:
-    TestScene(std::string name, Game* g) : Scene(name, g) {}
+    AutismoFields(std::string name, Game* g) : Scene(name, g) {
+        tex = IMG_LoadTexture(g->window->renderer, "res/gfx_desu.png");
+        if (tex == nullptr) {
+            std::cerr << "HOLY SHIT, FAILED TO LOAD THE TEXTURE! ABORT!" << std::endl;
+            abort();
+        }
+    }
     void Draw()
     {
-        float t = SDL_GetTicks() / 360.f;
-        SDL_Renderer* ren = game->window->renderer;
-        SDL_SetRenderDrawColor(ren, (pow(cos(t), 2)),
-                               255 * (pow(sin(t), 2)), 255, SDL_ALPHA_OPAQUE);
-        SDL_Rect r = {0, 0, 150, 150};
-        SDL_RenderFillRect(ren, &r);
-        SDL_SetRenderDrawColor(ren, 0, 0, 0, SDL_ALPHA_OPAQUE);
+        SDL_Renderer * ren = game->window->renderer;
+        SDL_Rect grass = {0, 0, 32, 32};
+        SDL_Rect flower = {64, 0, 32, 32};
+        SDL_Rect smileychan = {32, 0, 32, 32};
+        for (int y = 0; y < 240 / 32 + 1; ++y) {
+            for (int x = 0; x < 320 / 32; ++x) {
+                SDL_Rect dst = {x * 32, y * 32, 32, 32};
+                SDL_RenderCopy(ren, tex, &grass, &dst);
+            }
+        }
+        SDL_Rect dst = {scpos.x, scpos.y, 32, 32};
+        SDL_RenderCopy(ren, tex, &smileychan, &dst);
+        for (auto f : flowers) {
+            SDL_Rect dst = {f.x * 32, f.y * 32, 32, 32};
+            SDL_RenderCopy(ren, tex, &flower, &dst);
+        }
     }
+    void Logic() {
+        const Uint8 *state = SDL_GetKeyboardState(NULL);
+        if (state[SDL_SCANCODE_LEFT]) {
+            scpos.x -= 2;
+        }
+        if (state[SDL_SCANCODE_RIGHT]) {
+            scpos.x += 2;
+        }
+        if (state[SDL_SCANCODE_UP]) {
+            scpos.y -= 2;
+        }
+        if (state[SDL_SCANCODE_DOWN]) {
+            scpos.y += 2;
+        }
+    }
+private:
+    SDL_Texture * tex;
+    Pos scpos = {196, 134};
 };
 
 int main()
 {
-    Window* w = new Window("Simmer softly something's boilin", 640, 480, false);
+    Window* w = new Window("Simmer softly something's boilin", 640, 480, false, 320, 240);
     w->max_fps = 60;
 
+    if (Mix_Init(MIX_INIT_MOD) != MIX_INIT_MOD) {
+        std::cerr << "Mix_Init fail, yet still twerks... Ok." << std::endl;
+    }
+    if (Mix_OpenAudio(44100, AUDIO_F32, 2, 4096) != 0) {
+        std::cerr << "HOLY SHIT, CAN'T OPEN AUDIO! BAIL!" << std::endl;
+        return 1;
+    }
+    Mix_Music* mus = Mix_LoadMUS("autismofields.xm");
+    if (mus == NULL) {
+        std::cout << "Music load fail" << std::endl;
+        return 1;
+    }
+    Mix_PlayMusic(mus, -1);
+    if (IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG) {
+        std::cout << "Epic fail." << std::endl;
+        return 1;
+    }
+
     Game g(w);
-    g.AddScene(new TestScene("test", &g));
+    g.AddScene(new AutismoFields("test", &g));
     g.SetScene("test");
     g.Cycle();
 
